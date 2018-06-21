e0291f6ba36afa51aab46ce87112243030b8621a
Rebeca de Buen Kalman <rdebuen@uw.edu>
2018-06-21 14:03:39 -0700

    update to geocoder tests

--- scripts/tests/test_universal_geocoder.py
+++ scripts/tests/test_universal_geocoder.py
@@ -36,8 +36,42 @@ class UniGeoTest(unittest.TestCase):
 
 		"""
 
+	def test_contains_point(self):
+		"""
+		Given any polygon and point that is known to be in the polygon
+		the geocoder correctly recognizes that the point is contained in the polygon.
 
+		"""
+	def test_correct_key(self):
+		"""
+		Given a sample of points with known classification within the geographies,
+		the geocoder correctly identifies the key for each geography.
+		"""
 
+	def test_correct_tuple(self):
+		"""
+		The final tuple includes the correct keys.
+		"""
+
+	# exceptions
+
+	def test_point_outside_city(self):
+		"""
+		Exception for when the given (lat, lon) is outside the geographical bounds
+		of the city
+		"""
+
+	def test_invalid_point(self):
+		"""
+		Exception for when lat or long values given are invalid.
+		"""
+	def test_boundary_point(self):
+		"""
+		Given criteria for boundary point classification (on polygon), the geocoder correctly 
+		classifies the point.
+		"""
+
+	def test_overflow_error(self):
 
 suite = unittest.TestLoader().loadTestsFromTestCase(UniGeoTest)
 _ = unittest.TextTestRunner().run(suite)
