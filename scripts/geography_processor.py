7b1a7a6a803c63113a4e27141ad9339217d7bd94
Darius Irani <dirani2@jhu.edu>
2018-06-27 18:20:48 -0700

    added a polygon object as a boundary for seattle, and did an intersection overlay with my previous outline of seattle. The reason was, there were some extra polygon points in the previous outline that was causing extra things to be added during a spatial join later in the process. Also renamed the coordinates column to geometry to keep consistency with the shapefile input. Added a directory in the processed directory for csv files specifically to go.

--- scripts/geography_processor.py
+++ scripts/geography_processor.py
@@ -2,14 +2,17 @@ import os
 import pandas as pd
 import geopandas as gpd
 import pdb
+import sys
+from fiona.crs import from_epsg
+from shapely.geometry import Polygon
 # import matplotlib.pyplot as plt
 
 
 # plt.rcParams["figure.figsize"] = [16, 9] #optional
 
-def main():
+def main(file_type):
     #shape file and correlation csv filepaths
-    KING_COUNTY_FP = 'seamo/data/raw/blkgrp10_shore/blkgrp10_shore.shp'
+    KING_COUNTY_FP = 'seamo/data/raw/shapefiles/blkgrp10_shore.shp'
     SEATTLE_FP = 'seamo/data/raw/SeattleCensusBlocksandNeighborhoodCorrelationFile.xlsx'
     king_county = os.path.join(os.pardir, KING_COUNTY_FP)
     seattle = os.path.join(os.pardir, SEATTLE_FP)
@@ -33,11 +36,11 @@ def main():
 
     #process King County correlation dataframe
     tract_blkgrp = kc_data.loc[:, 'GEO_ID_GRP'].str[6:].astype(str)
-    coordinates = kc_data.loc[:, 'geometry']
+    geometry = kc_data.loc[:, 'geometry']
     tract_blkgrp.to_frame(name='tract_blkgrp')
-    coordinates.to_frame(name='coordinates')
-    kc = pd.concat([tract_blkgrp, coordinates],axis=1)
-    kc.columns=['tract_blkgrp', 'coordinates']
+    geometry.to_frame(name='geometry')
+    kc = pd.concat([tract_blkgrp, geometry],axis=1)
+    kc.columns=['tract_blkgrp', 'geometry']
 
     #inner join on Seattle census tract/block groups
     kc_s_join = pd.merge(s, kc, left_on='tract_blkgrp',
@@ -45,19 +48,45 @@ def main():
 
     #convert pandas dataframe to geopandas dataframe
     crs = {'init': 'epsg:4326'}
-    gdf = gpd.GeoDataFrame(kc_s_join, crs=crs, geometry='coordinates')
+    gdf = gpd.GeoDataFrame(kc_s_join, crs=crs, geometry='geometry')
+    gdf.crs = from_epsg(4326)
+
+    #create boundary for seattle
+    rectangle = gpd.GeoDataFrame([Polygon([(-122.435896, 47.734000),
+        (-122.285766, 47.734000),
+        (-122.285766, 47.735004),
+        (-122.246627, 47.683255),
+        (-122.245314, 47.495860),
+        (-122.435896, 47.495860)])],
+        columns=['geometry'], geometry='geometry')
+    rectangle.crs = from_epsg(4326)
+    rectangle.crs = gdf.crs
+
+    #overlay boundary of seattle with current outline to remove noise
+    seattle = gpd.overlay(gdf, rectangle, how='intersection')
 
     #plot map
-    # gdf.plot(cmap="tab20b")
+    # seattle.plot(cmap="tab20b")
 
     #calculate centroids
-    data = pd.concat([gdf, gdf.coordinates.centroid], axis=1)
-    data.columns=['tract_blkgrp', 'coordinates', 'centroid']
+    data = pd.concat([seattle, seattle.geometry.centroid], axis=1)
+    data.columns=['tract_blkgrp', 'geometry', 'centroid']
 
     #write to csv file
-    PRCOESSED_DATA_FP = 'seamo/data/processed/SeattleCensusBlockGroups.csv'
-    fp = king_county = os.path.join(os.pardir, PRCOESSED_DATA_FP)
-    data.to_csv(fp)
+    if file_type == 0:
+        PRCOESSED_DATA_FP = 'seamo/data/processed/csv_files/SeattleCensusBlockGroups.csv'
+        fp = king_county = os.path.join(os.pardir, PRCOESSED_DATA_FP)
+        data.to_csv(fp)
+    elif file_type == 1:
+        PRCOESSED_DATA_FP = 'seamo/data/processed/json_files/SeattleCensusBlockGroups.geojson'
+        fp = king_county = os.path.join(os.pardir, PRCOESSED_DATA_FP)
+        data.to_file(fp, driver='GeoJSON')
 
 if __name__ == "__main__":
-    main()
+    try:
+        if sys.argv[1] == "csv":
+            main(0)
+        elif sys.argv[1] == "json":
+            main(1)
+    except:
+        main(0)
