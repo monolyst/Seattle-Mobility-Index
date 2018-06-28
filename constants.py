f61b3fdc95dbabb1f91c20548d606a9055eb4def
amandalynne <amandalynne.p@gmail.com>
2018-06-28 15:35:07 -0700

    Initiate constants.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+"""
+Constants to be used throughout the code base.
+"""
+import os
+
+# Integers
+
+
+# Strings
+## Column Names
+DISTANCE = 'distance'
+
+## Categories for basket
+POST_OFFICE = 'post_office'
+SUPERMARKET = 'supermarket'
+URBAN_VILLAGE = 'urban_village'
+
+# File names
