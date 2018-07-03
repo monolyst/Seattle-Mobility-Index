a1d1ea8dfe3432d7186c4aca5df6d54b996e5988
Darius Irani <dirani2@jhu.edu>
2018-07-02 17:02:26 -0700

    Fixed geocoder so that it outputs csv file with all geographies associated with a lat/lon pair. Modularized the geocoder_input script so it can be called from the geocoder.

--- scripts/core/geocoder_input.py
+++ scripts/core/geocoder_input.py
@@ -4,40 +4,23 @@ import geopandas as gpd
 from shapely.geometry import Point
 
 #Read in shapes files for block group, neighborhoods, zipcode, council district and urban villages
-DATADIR = os.path.join(os.getcwd(), '../../seamo/data/raw/shapefiles/')
+# DATADIR = os.path.join(os.getcwd(), '../../seamo/data/raw/shapefiles/')
 
-def make_dataframe(DATADIR):
-	blkgrp = gpd.read_file(os.path.join(DATADIR, "blkgrp10_shore.shp"))
-	blkgrp = blkgrp.loc[:, ('GEO_ID_GRP', 'geometry')]
-	blkgrp = blkgrp.to_crs({'init' :'epsg:4326'})
-	blkgrp.rename(columns={blkgrp.columns[0]: 'keys'}, inplace=True)
-	blkgrp['Index'] = int(1)
+def read_shapefile(shapefile, column_name, name, DATADIR):
+    shapefile = str(shapefile) + '.shp'
+    geography = gpd.read_file(os.path.join(DATADIR, shapefile))
+    geography = geography.loc[:, (column_name, 'geometry')]
+    geography = geography.to_crs({'init' :'epsg:4326'})
+    geography.columns = ['key', 'geometry']
+    geography['geography'] = str(name)
+    return geography
 
-	nbhd = gpd.read_file(os.path.join(DATADIR, "Neighborhoods.shp"))
-	nbhd_short = nbhd.loc[:, ('S_HOOD', 'geometry')]
-	nbhd_long = nbhd.loc[:, ('L_HOOD', 'geometry')]
-	nbhd_short.rename(columns={nbhd_short.columns[0]: 'keys'}, inplace=True)
-	nbhd_long.rename(columns={nbhd_long.columns[0]: 'keys'}, inplace=True)
-	nbhd_short['Index'] = int(2)
-	nbhd_long['Index'] = int(3)
-
-	zipcode = gpd.read_file(os.path.join(DATADIR, "zipcode.shp"))
-	zipcode = zipcode.loc[:, ('ZIPCODE', 'geometry')]
-	zipcode = zipcode.to_crs({'init' :'epsg:4326'})
-	zipcode.rename(columns={zipcode.columns[0]: 'keys'}, inplace=True)
-	zipcode['Index'] = int(4)
-
-	scc = gpd.read_file(os.path.join(DATADIR, "sccdst.shp"))
-	scc = scc.loc[:, ('SCCDST', 'geometry')]
-	scc = scc.to_crs({'init' :'epsg:4326'})
-	scc.rename(columns={scc.columns[0]: 'keys'}, inplace=True)
-	scc['Index'] = int(5)
-
-	urbanvillage = gpd.read_file(os.path.join(DATADIR, "DPD_uvmfg_polygon.shp"))
-	urbanvillage = urbanvillage.loc[:, ('UV_NAME', 'geometry')]
-	urbanvillage.rename(columns={urbanvillage.columns[0]: 'keys'}, inplace=True)
-	urbanvillage['Index'] = int(6)
-
-	#Concatenate all GeoDataFrames into a single data
-	reference = pd.concat([blkgrp, nbhd_short, nbhd_long, zipcode, scc, urbanvillage])
-	return reference
+def make_reference(DATADIR):
+    blkgrp = read_shapefile('blkgrp10_shore', 'GEO_ID_GRP', 'Block_Group', DATADIR)
+    nbhd_short = read_shapefile('Neighborhoods', 'S_HOOD', 'Neighborhood_Short', DATADIR)
+    nbhd_long = read_shapefile('Neighborhoods', 'L_HOOD', 'Neighborhood_Long', DATADIR)
+    zipcode = read_shapefile('zipcode', 'ZIPCODE', 'Zipcode', DATADIR)
+    council_district = read_shapefile('sccdst', 'SCCDST', 'Seattle_City_Council_District', DATADIR)
+    urban_village = read_shapefile('DPD_uvmfg_polygon', 'UV_NAME', 'Urban_Village', DATADIR)
+    reference = pd.concat([blkgrp, nbhd_short, nbhd_long, zipcode, council_district, urban_village])
+    return reference
--- scripts/core/geopandas_geocoder.py
+++ scripts/core/geopandas_geocoder.py
@@ -1,3 +1,7 @@
+"""
+command to run script is $ python geopandas_geocoder csv_file output_name
+"""
+
 import os
 import sys
 import pandas as pd
@@ -6,34 +10,41 @@ from shapely.geometry import Point
 import geocoder_input as gi
 
 #Read in shapes files for block group, neighborhoods, zipcode, council district and urban villages
-DATADIR = os.path.join(os.getcwd(), '../../seamo/data/raw/shapefiles/')
-PROCESSED_DIR = os.path.join(os.getcwd(), '../../seamo/data/processed/')
+DATADIR = os.path.join(os.pardir, os.pardir, 'seamo/data/raw/shapefiles/')
+PROCESSED_DIR = os.path.join(os.pardir, os.pardir, 'seamo/data/processed/')
 
 #Geocoder function
-def geocode(lat, long, reference):
-    lat_long = gpd.GeoDataFrame([Point(long, lat)], columns=['geometry'], geometry='geometry')
-    lat_long.crs = {'init' :'epsg:4326'}
-
-    df = gpd.sjoin(lat_long, reference, how='inner')
-    df = df.drop(columns='index_right')
-    df = df.sort_values(by='Index')
-    df.loc[-2] = lat
-    df.loc[-1] = long
-    df.sort_index(inplace=True)
-    labels = pd.DataFrame(columns=['Latitude', 'Longitude', 'Block Group', 'Neighborhood_short', 'Neighborhood_long', 'Zip Code', 'Council District', 'UrbanVillage'])
-    labels.loc[1] = df['keys'].tolist()
-
-    return labels
+def geocode(input_file, reference):
+    """ 
+    input_file.csv needs header lat, lon
+    """
+    data = pd.read_csv(input_file)
+    
+    data['geometry'] = data.apply(lambda x: Point((float(x[1]), float(x[0]))), axis=1)
+    data = gpd.GeoDataFrame(data, geometry='geometry')
+    data.crs = {'init' :'epsg:4326'}
+                                 
+    df = gpd.sjoin(data, reference, how = 'inner')
+    df = df.drop(columns = ['index_right'])
+    df = df.sort_values(by='geography')
+    df = pd.DataFrame(df)
+    df = df.drop(['geometry'],axis=1)
+    df = df.set_index(['lat', 'lon','geography'], append='key').unstack()
+    df.columns = df.columns.droplevel()
+    values = {'Block_Group': 'N/A', 'Neighborhood_Long': 'N/A', 'Neighborhood_Short': 'N/A',
+              'Seattle_City_Council_District': 'N/A', 'Urban_Village': 'N/A', 'Zipcode': 'N/A'}
+    df = df.fillna(value=values)
+    
+    return (df)
 
 def main(argv):
-    reference = gi.make_dataframe(DATADIR)
-    file = str(sys.argv[1]) + '.csv' # add directory where the file should be found
-    df = pd.read_csv(file)
-    decoded = decoded.append(geocode(df['lat'], df['long'], reference), axis=1)
+    reference = gi.make_reference(DATADIR)
+    input_file = str(sys.argv[1]) + '.csv' # add directory where the file should be found
+    output_file = str(sys.argv[2]) + '.csv'
+    decoded = geocode(input_file, reference)
+    # print(decoded)
 
-    print(decoded)
-
-    decoded.to_csv(PROCESSED_DIR)
+    decoded.to_csv(PROCESSED_DIR + output_file)
 
 
 if __name__ == "__main__":
