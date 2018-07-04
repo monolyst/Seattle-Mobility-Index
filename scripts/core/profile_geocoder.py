79cd77ea182b4fbb0279d88c8f2d3e53c9cc8b26
Darius Irani <dirani2@jhu.edu>
2018-07-03 21:01:29 -0700

    edited profiler code to sort on tottime instead of cumtime

--- scripts/core/profile_geocoder.py
+++ scripts/core/profile_geocoder.py
@@ -6,11 +6,13 @@ import geocoder as gg
 import geocoder_input as gi
 
 DATADIR = os.path.join(os.pardir, os.pardir, 'seamo/data/raw/shapefiles/')
-REFERENCE = gi.make_reference(DATADIR)
+PROCESSED_DIR = os.path.join(os.pardir, os.pardir, 'seamo/data/processed/')
+pickle_name = "reference.pickle"
+REFERENCE = gi.get_reference(DATADIR, PROCESSED_DIR, pickle_name)
 INFILE = str(sys.argv[1]) + ".csv"
 OUTFILE = str(sys.argv[2])
 
 cProfile.run('gg.geocode(os.path.join(os.pardir, os.pardir, "seamo/data/test/", INFILE), REFERENCE)', OUTFILE)
 p = pstats.Stats(OUTFILE)
-p.sort_stats('cumtime')
+p.sort_stats('tottime')
 p.print_stats()
