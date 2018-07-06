c274244fe4a0afa8f164b8be4afbd923ad5a728f
Darius Irani <dirani2@jhu.edu>
2018-07-06 15:08:23 -0700

    refined df_to_sql converted, and added databases directory to store dbfiles.

--- scripts/preproc/df_to_sql.py
+++ scripts/preproc/df_to_sql.py
@@ -2,11 +2,10 @@ import sqlite3
 import sys
 import re
 import os
-import pandas as pd
 
 def df_to_sql(df, table_name):
     db_file = os.path.join(os.pardir, os.pardir,
-        "seamo/data/processed/databases/", table_name + ".db")
+        "seamo/data/processed/databases/", str(table_name) + ".db")
     conn = sqlite3.connect(db_file)
     df.to_sql(table_name, conn, schema=None, if_exists='fail')
     conn.commit()
