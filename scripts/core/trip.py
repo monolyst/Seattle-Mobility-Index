0e52079b269afc2f383e9adcef234c9422a1ad81
Darius Irani <dirani2@jhu.edu>
2018-07-09 16:38:25 -0700

    started trip class

---
+++ scripts/core/trip.py
@@ -0,0 +1,13 @@
+
+class Trip(object):
+	def __init__(self, origin, destination)
+	self.origin
+	self.
+
+# geocode itself
+
+trip.distance, trip.duration, trip.cost, trip.origin
+
+departure_time, distance_value,	duration_value,	duration_in_traffic_value, 
+end_lat, end_lon, fare_value, mode, origin, pair, place_id, rank, start_lat,
+start_lon, status
