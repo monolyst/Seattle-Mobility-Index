a441b7df3170af838ff7d235acf96d38c347657f
HJ Kim <kimh11@uw.edu>
2018-07-11 11:06:35 -0700

    Change scripts to seamo and move data folder to the new seamo

---
+++ seamo_old/seamo.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import pandas as pd
+import scipy.optimize as opt
+
+
+# Don't do anything.
+
+if __name__ == "__main__":
+    pass
