c8ae31de0cb4004b6f7032c82dd2259139c6bdb0
HJ Kim <kimh11@uw.edu>
2018-07-11 10:35:42 -0700

    Change all constants in geocoder_input file

--- scripts/core/geocoder_input.py
+++ scripts/core/geocoder_input.py
@@ -11,19 +11,19 @@ import contants as cn
 def read_shapefile(shapefile, column_name, name, DATADIR):
     shapefile = str(shapefile) + '.shp'
     geography = gpd.read_file(os.path.join(DATADIR, shapefile))
-    geography = geography.loc[:, (column_name, 'geometry')]
-    geography = geography.to_crs({'init' :'epsg:4326'})
-    geography.columns = ['key', 'geometry']
-    geography['geography'] = str(name)
+    geography = geography.loc[:, (column_name, cn.GEOMETRY)]
+    geography = geography.to_crs(cn.CRS_EPSG)
+    geography.columns = [cn.KEY, cn.GEOMETRY]
+    geography[cn.GEOGRAPHY] = str(name)
     return geography
 
 def make_reference(DATADIR, directory, pickle_name):
-    BLOCK_GROUP = read_shapefile('BLKGRP_FNAME', 'GEO_ID_GRP', 'Block_Group', DATADIR)
-    NBHD_SHORT = read_shapefile('Neighborhoods', 'S_HOOD', 'Neighborhood_Short', DATADIR)
-    NBHD_LONG = read_shapefile('Neighborhoods', 'L_HOOD', 'Neighborhood_Long', DATADIR)
-    ZIPCODE = read_shapefile('zipcode', 'ZIPCODE', 'Zipcode', DATADIR)
-    COUNCIL_DISTRICT = read_shapefile('sccdst', 'SCCDST', 'Seattle_City_Council_District', DATADIR)
-    URBAN_VILLAGE = read_shapefile('DPD_uvmfg_polygon', 'UV_NAME', 'Urban_Village', DATADIR)
+    blkgrp = read_shapefile(cn.BLKGRP_FNAME, cn.BLKGRP_KEY, cn.BLOCK_GROUP, DATADIR)
+    nbhd_short = read_shapefile(cn.NBHD_FNAME, cn.NBHD_SHORT_KEY, cn.NBHD_SHORT, DATADIR)
+    nbhd_long = read_shapefile(cn.NBHD_FNAME, cn.NBHD_LONG_KEY, cn.NBHD_LONG, DATADIR)
+    zipcode = read_shapefile(cn.ZIPCODE_FNAME, cn.ZIPCODE_KEY, cn.ZIPCODE, DATADIR)
+    council_district = read_shapefile(cn.COUNCIL_DISTRICT_FNAME, cn.COUNCIL_DISTRICT_KEY, cn.COUNCIL_DISTRICT, DATADIR)
+    urban_village = read_shapefile(cn.URBAN_VILLAGE_FNAME, cn.URBAN_VILLAGE_KEY, cn.URBAN_VILLAGE, DATADIR)
     reference = pd.concat([blkgrp, nbhd_short, nbhd_long, zipcode, council_district, urban_village])
     make_pickle(directory, reference, pickle_name)
     return reference
