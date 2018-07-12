0b6de7e5da73ffa2aafcdff3d39f31d41169560c
amandalynne <amandalynne.p@gmail.com>
2018-07-12 12:19:36 -0700

    refine haversine distance function

--- seamo/support/coordinate.py
+++ seamo/support/coordinate.py
@@ -8,19 +8,21 @@ class Coordinate:
     """
     def __init__(self, lat, lon):
         """
-        Initialize with a latitude and a longitude.
+        Initialize Coordinate with a latitude and a longitude (both floats).
         """
         self.lat = lat
         self.lon = lon
 
+
     def __str__(self):
         """
+        Stringify a Coordinate.
         Format is 'lat,lon'
         """
         return "{0},{1}".format(self.lat, self.lon)
 
 
-    def distance_haversine(self, coordinate):
+    def haversine_distance(self, coordinate):
         """
         Calculate haversine distance between this Coordinate
         and another Coordinate.
@@ -28,16 +30,20 @@ class Coordinate:
         input: coordinate (Coordinate)
         output: distance (float)
         """
-        
+       
+        # This Coordinate 
         lat1 = radians(self.lat)
         lon1 = radians(self.lon)
+        # The Coordinate to calculate distance to
         lat2 = radians(coordinate.lat)
         lon2 = radians(coordinate.lon)
 
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
+        # Differences
+        diff_lon = lon2 - lon1
+        diff_lat = lat2 - lat1
 
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        # Solve for distance applying inverse Haversine
+        a = sin(diff_lat / 2)**2 + cos(lat1) * cos(lat2) * sin(diff_lon / 2)**2
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
         distance = cn.EARTH_RADIUS_KM * c 
