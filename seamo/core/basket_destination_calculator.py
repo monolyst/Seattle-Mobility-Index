9c4bb3014ca62060f20e58996f4d15bd6ba1c360
amandalynne <amandalynne.p@gmail.com>
2018-07-11 17:24:02 -0700

    more refactor and kill a bug

--- seamo/core/basket_destination_calculator.py
+++ seamo/core/basket_destination_calculator.py
@@ -1,26 +1,27 @@
 # coding: utf-8
 """
 Basket Destination Calculator
- 
-This script constructs a market basket of destinations relevant to people 
-who travel in Seattle. The basket may nearby points of interest and activity 
-centers that are specific to each origin, and citywide destinations 
-that are the same for all starting points.  
+
+This script constructs a market basket of destinations relevant to people
+who travel in Seattle. The basket may nearby points of interest and activity
+centers that are specific to each origin, and citywide destinations
+that are the same for all starting points. 
 
 https://public.tableau.com/views/Basket_of_Destinations/Dashboard?:embed=y&:display_count=yes
 
-This script accesses the Google Map Distance Matrix API and ranks each 
-possible origin-destination pair by their driving distance. 
-The basket definition is created by using parameters to filter each 
+This script accesses the Google Map Distance Matrix API and ranks each
+possible origin-destination pair by their driving distance.
+The basket definition is created by using parameters to filter each
 class of destination.
 """
-import __init__
 import json
-import os
 from urllib.request import Request, urlopen
 
-import constants as cn
 import pandas as pd
+
+import constants as cn
+import __init__
+
 
 
 class Coordinate:
@@ -49,8 +50,8 @@ class BasketCalculator:
 
     def __init__(self, api_key):
         """
-        Initialize BasketCalculator with an API key. 
-        
+        Initialize BasketCalculator with an API key.
+       
         Inputs: api_key (string)
         """
         self.api_key = api_key
@@ -58,31 +59,31 @@ class BasketCalculator:
 
     def origins_to_distances(self, origin_df=origin_df, dest_df=dest_df):
         """
-        For every origin, store the distance to every destination in the 
+        For every origin, store the distance to every destination in the
         full basket of destinations. Distance is calculated via the Google
         Distance Matrix API.
 
         Inputs: origin_df (dataframe), dest_df (dataframe)
         Outputs: dist_df (dataframe)
-        
+       
         """
-        dist_matrix = [] 
+        dist_matrix = []
         for i, row in origin_df.iterrows():
             blockgroup = row[cn.BLOCKGROUP]
             origin = Coordinate(row[cn.CENSUS_LAT], row[cn.CENSUS_LON])
-            distances = self.calculate_distance_to_basket(origin, dest_df) 
+            distances = self.calculate_distance_to_basket(origin, dest_df)
             for place_id, data in distances.items():
                 distance = data[cn.DISTANCE]
                 dest_class = data[cn.CLASS]
-                pair = "{0}-{1}".format(blockgroup, place_id) 
+                pair = "{0}-{1}".format(blockgroup, place_id)
                 dist_matrix.append([pair, distance, dest_class])
 
         dist_df = pd.DataFrame(dist_matrix, columns=[cn.PAIR, cn.DISTANCE, cn.CLASS])
-    
+   
         # rank it by proximity
         dist_df = self.rank_destinations(dist_df)
 
-        return dist_df 
+        return dist_df
 
 
     def rank_destinations(self, dist_df):
@@ -94,7 +95,7 @@ class BasketCalculator:
         """
         # Group by blockgroup and destination class
         grouped = dist_df.groupby([cn.BLOCKGROUP, cn.CLASS])
-        # Rank by proximity (closest is highest) 
+        # Rank by proximity (closest is highest)
         dist_df[cn.RANK] = grouped[cn.DISTANCE].rank(
             ascending=True, method='first')
         return dist_df
@@ -104,12 +105,12 @@ class BasketCalculator:
         """
         Calculate the distance between an origin and destination pair.
         Calls Google Distance Matrix API.
-    
+   
         Input:  origin (Coordinate)
                 destination (Coordinate)
         Output: distance in miles (int)
-        """ 
-        distance = 0 
+        """
+        distance = 0
 
         url = cn.DIST_MATRIX_URL +\
               'units={0}'.format(cn.IMPERIAL_UNITS) +\
@@ -118,10 +119,10 @@ class BasketCalculator:
               "&destinations={0}".format(str(destination)) +\
               "&key={0}".format(api_key)
         request = Request(url)
-        try: 
+        try:
             response = urlopen(request).read()
         except:
-            raise Exception("Couldn't open link.")  
+            raise Exception("Couldn't open link.") 
 
         data = json.loads(response)
 
@@ -134,11 +135,11 @@ class BasketCalculator:
             element = elements[0]
             if element['status'] == 'NOT_FOUND':
                 # If the origin-destination pair is not found, should write to a log.
-                raise Exception('Could not find the distance for that pair.')  
+                raise Exception('Could not find the distance for that pair.') 
             elif element['status'] == 'OK':
                 distance = element['distance']['value']
 
-        return distance 
+        return distance
 
 
     def calculate_distance_to_basket(self, origin, dest_df):
@@ -152,11 +153,11 @@ class BasketCalculator:
         Output:
             distances (dict)
 
-        """ 
+        """
         distances = {}
 
         for index, row in dest_df.iterrows():
-            destination = Coordinate(row[cn.GOOGLE_PLACES_LAT], 
+            destination = Coordinate(row[cn.GOOGLE_PLACES_LAT],
                                      row[cn.GOOGLE_PLACES_LON])
             dest_class = row[cn.CLASS]
             place_id = row[cn.PLACE_ID]
@@ -172,16 +173,16 @@ class BasketCalculator:
 
     def create_basket(self, origin_df, basket_combination):
         """
-        Given a list of integers denoting counts for basket categories 
-        and a dataframe of origins (blockgroups), create a basket of destinations 
-        for each blockgroup. 
+        Given a list of integers denoting counts for basket categories
+        and a dataframe of origins (blockgroups), create a basket of destinations
+        for each blockgroup.
 
         Input: origin_df (dataframe), basket_combination (list)
         Output: dataframe
         """
 
-        for i, category in enumerate(cn.BASKET_CATEGORIES)):
-            origin_df = origin_df[(origin_df[cn.CLASS] != category) | 
+        for i, category in enumerate(cn.BASKET_CATEGORIES):
+            origin_df = origin_df[(origin_df[cn.CLASS] != category) |
                 (origin_df[cn.RANK] <= basket_combination[i])]
 
         return origin_df
@@ -197,7 +198,7 @@ if __name__ == "__main__":
     dest_df = BasketCalculator.dest_df
 
     distance_df = basket_calculator.origins_to_distances(origin_df, dest_df)
-    # Is it a problem that this is all stored in memory until write to file?    
+    # Is it a problem that this is all stored in memory until write to file?   
 
     output_fp = "basket.csv"
     distance_df.to_csv(output_fp)
