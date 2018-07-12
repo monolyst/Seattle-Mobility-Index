2b597e4719d710206470cf9ec643b411ccfe246f
Darius Irani <dirani2@jhu.edu>
2018-07-12 11:56:23 -0700

    added rank to trip class.

--- seamo/support/trip.py
+++ seamo/support/trip.py
@@ -2,7 +2,7 @@
 Base Trip Class
 """
 class Trip(object):
-    def __init__(self, origin, destination, mode, distance, duration, category, pair, departure_time):
+    def __init__(self, origin, destination, mode, distance, duration, category, pair, departure_time, rank):
         self.origin = origin
         self.destination = destination
         self.mode = mode
@@ -10,6 +10,7 @@ class Trip(object):
         self.duration = calculate_duration(duration)
         self.category = category
         self.cost = calculate_cost(mode)
+        self.rank = rank
         self.persona = None
         self.time_of_day = None
         self.type_of_day = None
@@ -28,17 +29,16 @@ class Trip(object):
         #parse departure time
         self.departure_time = str(origin.block_group()) + '_' + pair + mode # + parsed departure time
 
-# geocode itself
 
 class CarTrip(Trip):
-    def __init__(self, origin, destination, mode, distance, category, duration_in_traffic):
+    def __init__(self, origin, destination, mode, distance, category, duration_in_traffic=0):
         super().__init__(self, origin, destination, mode="car", distance, category)
         self.duration = calculate_duration(duration, duration_in_traffic)
 
     def calculate_cost(self, mile_rate, parking, toll_data=0):
         self.cost = self.distance * mile_rate + parking + toll_data
 
-    def calculate_duration(duration, duration_in_traffic):
+    def calculate_duration(duration, duration_in_traffic=0):
         # Not sure if this right
         self.duration = duration + duration_in_traffic
 
