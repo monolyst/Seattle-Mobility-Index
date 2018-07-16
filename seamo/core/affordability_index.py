554ca3bfda9b482b2c7195e44901956b34b58a4b
Darius Irani <dirani2@jhu.edu>
2018-07-16 10:33:35 -0700

    minor changes

--- seamo/core/affordability_index.py
+++ seamo/core/affordability_index.py
@@ -11,14 +11,6 @@ class AffordabilityIndex(index_base_class):
             db_name=cn.GOOGLE_DIST_MATRIX_OUT, datadir=DATADIR)
         self.travel_cost = travel_cost
 
-    def query_trip_data(self, db_name=cn.GOOGLE_DIST_MATRIX_OUT):
-        db_file = os.path.join(cn.DB_DIR, db_name + '.db')
-        conn = sqlite3.connect(db_file)
-        df = read_sql_table(db_file, conn)
-        conn.commit()
-        conn.close()
-        return df
-
 
     def get_data(self, filename, datadir=DATADIR):
         df = pd.read_csv(str(filename) + '.csv')
