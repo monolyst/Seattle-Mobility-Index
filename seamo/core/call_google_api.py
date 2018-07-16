335700cd2edaed49e54295a5dfac4cfe2d86bf5f
amandalynne <amandalynne.p@gmail.com>
2018-07-16 10:20:44 -0700

    only write data if distance is not 0

--- seamo/core/call_google_api.py
+++ seamo/core/call_google_api.py
@@ -25,13 +25,18 @@ if __name__ == "__main__":
     distance_df = pd.read_csv(cn.HAVERSINE_DIST_FP) 
 
     if day_of_week == "Friday":
-        subset_df = distance_df.iloc[:80000]
+        # subset_df = distance_df.iloc[:80000]
+        # subset_df = distance_df.iloc[51165:80000]
+        pass
     elif day_of_week == "Saturday":
-        subset_df = distance_df.iloc[80000:180000]
+        # subset_df = distance_df.iloc[80000:180000]
+        subset_df = distance_df.iloc[55785:155785]
     elif day_of_week == "Sunday":
-        subset_df = distance_df.iloc[180000:280000]
+        # subset_df = distance_df.iloc[165707:255785]
+        # subset_df = distance_df.iloc[180000:280000]
+        subset_df = distance_df.iloc[188342:300000]
     else:
-        subset_df = distance_df.iloc[280000:]
+        subset_df = distance_df.iloc[255785:]
 
     cols = [cn.BLOCKGROUP, cn.PAIR, cn.DISTANCE, cn.CLASS,
             cn.GOOGLE_START_LAT, cn.GOOGLE_START_LON,
@@ -54,14 +59,15 @@ if __name__ == "__main__":
             end_lon = row[cn.GOOGLE_END_LON]
             destination = Coordinate(end_lat, end_lon)
             distance = bc.calculate_distance_API(origin, destination)
-            dest_class = row[cn.CLASS]
-            end_lat = row[cn.GOOGLE_END_LAT]
-            end_lon = row[cn.GOOGLE_END_LON]
-            pair = row[cn.PAIR] 
-            row = [blockgroup, pair, distance, dest_class,
-                   origin_lat, origin_lon, end_lat, end_lon]
+            if distance: 
+                dest_class = row[cn.CLASS]
+                end_lat = row[cn.GOOGLE_END_LAT]
+                end_lon = row[cn.GOOGLE_END_LON]
+                pair = row[cn.PAIR] 
+                row = [blockgroup, pair, distance, dest_class,
+                       origin_lat, origin_lon, end_lat, end_lon]
 
-            outf.write(','.join([str(e) for e in row]))
-            outf.write('\n')
+                outf.write(','.join([str(e) for e in row]))
+                outf.write('\n')
        
 
