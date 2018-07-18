000024f4251dcc8992ff03a6ecf87db6095e2c1d
amandalynne <amandalynne.p@gmail.com>
2018-07-18 11:51:29 -0700

    create skeleton for basket calc tests

---
+++ seamo/tests/test_basket_calculator.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class BasketCalcTest(unittest.Testcase):
+    def test_calculate_distance_API():
+        def reader():
+            # return a JSON string
+
+
+    def test_basket_rank():
+        # Check columns
+        # Check ranks are integers
+        # Check size of input and output match
+        pass
+
+
+    def test_create_basket():
+        # Check that each blockgroup has a basket of 25 
+        # Check that the count for each category matches basket
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+
