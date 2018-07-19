13751d88ac89705dafa3c3e872fc3de982cdb267
amandalynne <amandalynne.p@gmail.com>
2018-07-19 16:18:37 -0700

    add logic for constraining API calls and maintaining data queue

--- seamo/core/main_basket_calculator.py
+++ seamo/core/main_basket_calculator.py
@@ -41,16 +41,20 @@ if not os.path.exists(OUTPUT_FP):
         outf.write(','.join(cols))
         outf.write('\n')
 
-# Need to look at index of last row
-# count lines that are in cn.API_DIST_FP
-# count lines that are in error_log
+dist_df = pd.read_csv(cn.HAVERSINE_DIST_FP)
 
-# How to keep ones "on the stack" that didn't work?
-subset_df = dist_df[index:] 
+# If the queue does not exist, create it by making a copy of the distance csv
+if not os.path.exists(cn.DISTANCE_QUEUE_FP):
+    dist_df.to_csv(cn.DISTANCE_QUEUE_FP)
+
+# If the 'stack' file exists, use it
+queue_df = pd.read_csv(cn.DISTANCE_QUEUE_FP)  
+
+api_calls = 0
+finished_rows = []
 
 with open(OUTPUT_FP, 'a+') as outf:
-    # Need to create subset_df
-    for index, row in subset_df.iterrows():
+    for index, row in queue_df.iterrows():
         blockgroup = row[cn.BLOCKGROUP]
         origin_lat = row[cn.GOOGLE_START_LAT]
         origin_lon = row[cn.GOOGLE_START_LON]
@@ -59,7 +63,11 @@ with open(OUTPUT_FP, 'a+') as outf:
         end_lon = row[cn.GOOGLE_END_LON]
         destination = Coordinate(end_lat, end_lon)
         distance = bc.calculate_distance_API(origin, destination)
+        # Increment number of API calls made
+        api_calls +=1 
         if distance: 
+            # If the call was successful, log its index
+            finished_rows.append(index)
             dest_class = row[cn.CLASS]
             end_lat = row[cn.GOOGLE_END_LAT]
             end_lon = row[cn.GOOGLE_END_LON]
@@ -69,36 +77,44 @@ with open(OUTPUT_FP, 'a+') as outf:
 
             outf.write(','.join([str(e) for e in row]))
             outf.write('\n')
-   
+        if api_calls == cn.API_CALL_LIMIT:
+            break
+
+# Remove rows for which we successfully got data 
+queue_df = queue_df.drop(queue_df.index[finished_rows])
+queue_df.to_csv(cn.DISTANCE_QUEUE_FP)
+
 api_distances = cn.API_DIST_FP
+
+
+# For each blockgroup and class of destination, rank by proximity.
 dist_df = pd.read_csv(api_distances)
 ranked_df = rank_destinations(dist_df)
-ranked_df.to_csv('ranked_destinations.csv')
+ranked_df.to_csv(cn.RANKED_DEST_FP)
 baskets_df = create_basket(ranked_df, cn.FINAL_BASKET)
-baskets_df.to_csv('baskets.csv')
+baskets_df.to_csv(cn.BASKETS_FP)
 # Also need to put out the compressed formats where dests are in one col
-bgs = { blockgroup: { 'origin' : None, 'destinations' : [], 'pair': None, 'class': None } for blockgroup in df['BLOCKGROUP'].values }
+bgs = { blockgroup: { cn.ORIGIN : None, cn.DESTINATIONS : [], cn.PAIR: None, cn.CLASS: None } for blockgroup in df[cn.BLOCKGROUP].values }
 for index, row in df.iterrows():
-    origin = "{0},{1}".format(row['start_lat'], row['start_lon'])
-    destination = "{0},{1}".format(row['end_lat'], row['end_lon'])
-    pair = row['pair']
+    origin = "{0},{1}".format(row[cn.GOOGLE_START_LAT], row[cn.GOOGLE_START_LON])
+    destination = "{0},{1}".format(row[cn.GOOGLE_END_LAT], row[cn.GOOGLE_END_LON])
+    pair = row[cn.PAIR]
 
-    
-    
-    bgs[row['BLOCKGROUP']]['destinations'].append(destination) 
-    bgs[row['BLOCKGROUP']]['origin'] = origin
-    bgs[row['BLOCKGROUP']]['pair'] = paircols = ['BLOCKGROUP', 'pair', 'origin', 'destinations']
+    bgs[row[cn.BLOCKGROUP]][cn.DESTINATIONS].append(destination) 
+    bgs[row[cn.BLOCKGROUP]][cn.ORIGIN] = origin
+    bgs[row[cn.BLOCKGROUP]][cn.PAIR] = paircols = [cn.BLOCKGROUP, cn.PAIR, cn.ORIGIN, cn.DESTINATIONS]
 rows = []
 new_df = pd.DataFrame(columns=cols)
 for bg, data in bgs.items():
-    origin = data['origin']
-    pair = data['pair']
-    dests = " | ".join(data['destinations'])
-    new_row = { 'BLOCKGROUP' : bg,
-                'origin': origin,
-                'pair': pair,
-                'destinations': dests
+    origin = data[cn.ORIGIN]
+    pair = data[cn.PAIR]
+    # Separate destinations with a pipe
+    dests = "|".join(data[cn.DESTINATIONS])
+    new_row = { cn.BLOCKGROUP : bg,
+                cn.ORIGIN: origin,
+                cn.PAIR: pair,
+                cn.DESTINATIONS: dests
                     }
     rows.append(new_row)
 new_df = pd.DataFrame(rows, columns=cols)
-new_df.to_csv('input_baskets.csv')
+new_df.to_csv(cn.INPUT_BASKETS_FP)
