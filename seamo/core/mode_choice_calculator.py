104a349760e9c806d2922c9724efa33d8e9318fd
Rebeca de Buen Kalman <rdebuen@uw.edu>
2018-07-23 16:56:18 -0700

    drafted viability function

--- seamo/core/mode_choice_calculator.py
+++ seamo/core/mode_choice_calculator.py
@@ -1,5 +1,32 @@
 import init
+import constants as cn
 from index_base_class import IndexBaseClass
 
 class ModeChoiceCalculator(IndexBaseClass):
-	pass
+	"""
+	"""
+
+
+	def __init__(self):
+		pass
+
+	def is_viable(self, trip):
+		if trip.mode == cn.CAR and trip.duration < 60:
+			return 1
+		elif trip.mode == cn.BIKE and trip.duration < 60:
+			return 1
+		
+		elif trip.mode == cn.TRANSIT and trip.duration < 60:
+			return 1
+
+		elif trip.mode == cn.WALK and trip.duration < 45:
+			return 1
+
+			#can we take into account proximity? thinking of nearby locations with bad connections
+			# or disnant locations with good connections. Most relevant for bus.
+
+		#Make threasholds constants
+		
+		else:
+		return 0
+
