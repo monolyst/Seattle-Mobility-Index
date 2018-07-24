766f6e873a1ba0fea34c595d46a08b4448f6efee
Rebeca de Buen Kalman <rdebuen@uw.edu>
2018-07-24 12:06:24 -0700

    round 3 of indentation fix

--- seamo/core/mode_choice_calculator.py
+++ seamo/core/mode_choice_calculator.py
@@ -3,28 +3,28 @@ import constants as cn
 from index_base_class import IndexBaseClass
 
 class ModeChoiceCalculator(IndexBaseClass):
-	"""
-	"""
-	def __init__(self):
-		pass
+    """
+    """
+    def __init__(self):
+        pass
 
-	def is_viable(self, trip):
-		viable = 0
-		if trip.mode == cn.CAR and trip.duration < 60:
-			viable = 1
-		elif trip.mode == cn.BIKE and trip.duration < 60:
-			viable = 1
-		elif trip.mode == cn.TRANSIT and trip.duration < 60:
-			viable = 1
-		elif trip.mode == cn.WALK and trip.duration < 45:
-			viable = 1
+    def is_viable(self, trip):
+        viable = 0
+        if trip.mode == cn.CAR and trip.duration < 60:
+            viable = 1
+        elif trip.mode == cn.BIKE and trip.duration < 60:
+            viable = 1
+        elif trip.mode == cn.TRANSIT and trip.duration < 60:
+            viable = 1
+        elif trip.mode == cn.WALK and trip.duration < 45:
+            viable = 1
 
-			#can we take into account proximity? thinking of nearby locations with bad connections
-			# or disnant locations with good connections. Most relevant for bus.
+            #can we take into account proximity? thinking of nearby locations with bad connections
+            # or disnant locations with good connections. Most relevant for bus.
 
-		#Make threasholds constants
-		
-		return viable
+        #Make threasholds constants
+        
+        return viable
 
 
 
