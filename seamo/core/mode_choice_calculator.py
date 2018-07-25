a6bb11a3ec05f82c15cba8765af82c264c690860
Rebeca de Buen Kalman <rdebuen@uw.edu>
2018-07-25 11:38:40 -0700

    added functions to mode_choice_calculator

--- seamo/core/mode_choice_calculator.py
+++ seamo/core/mode_choice_calculator.py
@@ -1,6 +1,11 @@
 import init
 import constants as cn
 from index_base_class import IndexBaseClass
+from trip import Trip
+
+import pandas as pd
+
+
 
 class ModeChoiceCalculator(IndexBaseClass):
     """
@@ -26,6 +31,77 @@ class ModeChoiceCalculator(IndexBaseClass):
         #Make threasholds constants
         
         return viable
+
+    
+    def csv_to_dataframe(self, csv_filepath):
+        df =pd.read_csv(csv_filepath)
+
+        return df
+
+    
+    def extract_blkgrp(self, trip_id):
+        blkgrp = trip_id.split('++')[0]
+
+        return blkgrp
+
+    
+    def extract_dest(self, trip_id):
+        dest = trip_id.split('++')[1]
+
+        return dest
+
+
+    def trip_from_row(self, row):
+        """
+        Given a dataframe with the available variables, create a trip class 
+        with these variables
+        """
+
+        #Need to change strings for constants
+        trip_id = row['trip_id']
+        origin = self.extract_blkgrp(trip_id)
+        destination = self.extract_dest(trip_id)
+        mode = row['mode']
+        distance = row['distance']
+        duration = row['duration']
+        basket_category = None
+        pair = None
+        #convert departure time to date-time object
+        departure_time = row['departure_time']
+        rank = None
+
+        trip = Trip(trip_id, origin, destination, mode, distance, duration, basket_category, 
+            pair, departure_time, rank)
+        return trip
+
+    
+    def create_blockgroup_dict(self, df):
+
+        """
+        this function assumes that the df has a column for trip viability
+        """
+        blkgrp_dict = defaultdict(list)
+        for index, row in df.iterrows():
+            trip = self.trip_from_row(row)
+            blkgrp = trip.origin
+            viable= row['viable']
+            #need a way to add viable to trip
+            blkgrp_dict[blkgrp].append(trip)
+
+        return blkgrp_dict
+
+
+    def calculate_mode_avail(self, trips):
+        """
+        trips is a list of Trips
+
+        """
+        #make sure trip has viable attribute
+        # Hours of data availability, HOURS constant should be float
+        mode_avail = sum([trip.viable for trip in trips])
+        mode_index = mode_aval/cn.HOURS #(?) name constant
+
+
 
     def calculate_availability(self, blkgrp):
         #for every block group
