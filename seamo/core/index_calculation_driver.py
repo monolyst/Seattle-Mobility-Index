fa1562b5046b4eb8d72e49760fcefc893ce80999
amandalynne <amandalynne.p@gmail.com>
2018-07-31 10:37:44 -0700

    remove duplicate drips from dataframe

--- seamo/core/index_calculation_driver.py
+++ seamo/core/index_calculation_driver.py
@@ -12,6 +12,10 @@ mc = ModeChoiceCalculator()
 ac = AffordabilityIndex()
 
 total_trips_df = pd.read_csv(cn.DISTANCES_OUT_FP)
+# Need to drop the duplicates.
+to_drop = total_trips_df[((total_trips_df.destination == "First Hill") | (total_trips_df.destination == "South Lake Union")) & (total_trips_df['class'] == "urban_village")]
+
+total_trips_df = total_trips_df.drop(to_drop.index)
 
 # Create a dictionary where keys are blockgroup IDs and values are lists of Trips
 trips_per_blockgroup = mc.trips_per_blockgroup(total_trips_df)
