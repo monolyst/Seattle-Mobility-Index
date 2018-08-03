1418658d029bbf175e0d6dfae4fb697e1bca6c5b
amandalynne <amandalynne.p@gmail.com>
2018-08-03 14:38:45 -0700

    add script that produces mode viability scores per persona

--- seamo/core/persona_avail.py
+++ seamo/core/persona_avail.py
@@ -1,3 +1,13 @@
+"""
+Usage: (from seamo/) python core/persona_avail.py
+
+This script creates a ModeChoiceCalculator for each Persona and produces
+mode viability scores for each blockgroup for each Persona based on their 
+unique thresholds.
+
+These scores are saved in individual CSV files in the seamo/data/processed/csv_files
+folder.
+"""
 import init
 import os
 import constants as cn 
@@ -10,10 +20,9 @@ import time
 import os
 import pandas as pd
 
-
 PERSONA_DICTS = pd.read_csv(cn.SAMPLE_PERSONA_FP, index_col=0).to_dict('index')
 
-total_trips_df = pd.read_csv(os.path.join(cn.CSV_DIR, 'google_dist_matrix_out.csv'))
+total_trips_df = pd.read_csv(cn.DISTANCES_OUT_FP)
 # Need to drop the duplicates.
 # First Hill and SLU are only 'citywide' destinations in the baskets,
 # never 'urban_village'
@@ -23,21 +32,15 @@ to_drop = total_trips_df[((total_trips_df.destination == "First Hill") |
 
 total_trips_df = total_trips_df.drop(to_drop.index)
 
-p0 = PERSONA_DICTS['family-(wo)man'] 
-p1 = PERSONA_DICTS['fit-urbanites'] 
-p2 = PERSONA_DICTS['tired-commuter'] 
-p3 = PERSONA_DICTS['jolly-retiree']
-p4 = PERSONA_DICTS['olde-Seattleite']
-
 for persona, attrs in PERSONA_DICTS.items():
     driving_threshold = attrs['driving_threshold']
     transit_threshold = attrs['transit_threshold'] 
     biking_threshold = attrs['biking_threshold']
     walking_threshold = attrs['walking_threshold']
     mc = ModeChoiceCalculator(driving_threshold,
-                              transit_threshold,
                               biking_threshold,
+                              transit_threshold,
                               walking_threshold)
     trips = mc.trips_per_blockgroup(total_trips_df)
     avail_df = mc.create_availability_df(trips) 
-    avail_df.to_csv('mode_availability_{0}.csv'.format(persona))
+    avail_df.to_csv(os.path.join(cn.CSV_DIR, 'mode_availability_{0}.csv'.format(persona)))
